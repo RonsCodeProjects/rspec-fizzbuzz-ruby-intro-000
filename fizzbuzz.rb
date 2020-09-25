b10e09cdf41dc532fb449ec049d1aa5d227ef5ed
Ronald Colonna <roncolonna@yahoo.com>
2020-09-25 23:07:08 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,6 +1,16 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz
+def fizzbuzz(num)
+  
+  If num % 3 == 0 
+  
+    "Fizz"
+    
+  elseif int % 5 == 0
+
+    "Buzz"
+    
+  elseif 
   
 end
